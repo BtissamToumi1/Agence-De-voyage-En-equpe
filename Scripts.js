27cb2faf184e0ae354b9fd396e52d5f13c022336
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-19 00:03:30 +0100

    creation des variables pour recuperer les donnes de formualire

---
+++ Scripts.js
@@ -0,0 +1,7 @@
+//variables
+const nominput=document.querySelector("#exampleInputNom1");
+const prenominput=document.querySelector("#exampleInputPrenom1");
+const telephoneinput=document.querySelector("#exampleInputTelephone1");
+const emailinput=document.querySelector("#exampleInputEmail1");
+const buutton=document.querySelector(".btn");
+const contactlist=document.querySelector("#contact-list");
